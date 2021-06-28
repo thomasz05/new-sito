9b5bdecf0d984ff2c75f68a995c14c1d3c309cdb
thomasz05 <thomaszamengo@gmail.com>
2021-06-29 00:24:03 +0200

    redcraft.js

--- redcraft/red.js
+++ redcraft/red.js
@@ -11,14 +11,15 @@ function pingServer () {
         // ERROR
         if (response.error) {
             // PLAYER COUNT
-            players.text("Server offline");
+            players.text("Server offline ");
+            document.getElementById("status").style.color = "red";
 
             // EMPTY TABLE
             playerList.html('');
             $('<tr>').append(
                 $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
                 $('<td>').text('NONE'),
-                $('<td>').text('NONE')
+                $('<td>').text('NONE'),
             ).appendTo(playerList);
 
             return
@@ -28,13 +29,15 @@ function pingServer () {
 
         // PLAYER COUNT
         players.text("Giocatori Online: " + response.players.online + " / " + response.players.max);
+        document.getElementById("status").style.color = "green";
         if (response.players.online==0) {
             // EMPTY SERVER
             playerList.html('');
+            
             $('<tr>').append(
                 $('<td>').html(`<img src='https://minotar.net/helm/HeroBrine/20'>`),
                 $('<td>').text('NONE'),
-                $('<td>').text('NONE')
+                $('<td>').text('NONE'),
             ).appendTo(playerList);
             return
         }
